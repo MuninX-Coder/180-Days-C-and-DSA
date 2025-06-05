375ad9fb24612b0e3b7146cec712847f3410dff6
MuninX-Coder <muninborah1825@gmail.com>
2025-06-05 06:51:16 +0530

    Add files via upload

---
+++ Day 1/Day_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>          // #include <iostream>  means that there is a file name iostream and now include it and be readay to use it (header file)
+using namespace std;         // this line define that use this fuction only, means it use it directly from the std.
+
+int main(){                  // int means integers and main is a function, main is the entry point of our programmer. all the programmer will be under main().
+    cout<<"Hello world \n"; 
+    cout<<"Hello India";    // cout is used to display the output. it will show hello world in the screen.
+    return 0;                 // now return 0. used to indicate that the programme is executed successfully without any error
+}
+
+// THIS IS A SINGLE LINE COMMENTS
+
+/*
+
+THIS 
+IS A 
+MULTIPLE 
+LINE COMMENTS
+
+*/
+
+// 1. Ek simple C++ program likho jo "Hello, India!" print kare.
+
+//2. Ek program likho jo do alag-alag cout statements se output de:
